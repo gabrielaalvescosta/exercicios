b5ee4546ccdc55647919989e916a09681c6b72f4
Gabriela Costa <gabrielasilvaalvescosta@gmail.com>
2022-04-01 20:11:08 -0300

    Ex 8 complemento

--- python/lista-3/exercicio8.py
+++ python/lista-3/exercicio8.py
@@ -1,12 +1,14 @@
 age = int(input('Idade: '))
 
-if age <= 7:
+if age < 5:
+    print('Não existe categoria para esta idade.')
+elif age <= 7:
     print('Categoria Infantil')
-elif age >= 8 and age <= 10:
+elif age <= 10:
     print('Categoria Juvenil')
-elif age >= 11 and age <= 15:
+elif age <= 15:
     print('Categoria Adolescente')
-elif age >= 16 and age <= 30:
+elif age <= 30:
     print('Categoria Adulta')
 elif age > 30:
     print('Categoria Senior')
