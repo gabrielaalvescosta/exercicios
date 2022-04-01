3d65391ef25979a54fda9a27a87f6fb27ad78550
Gabriela Costa <gabrielasilvaalvescosta@gmail.com>
2022-04-01 19:50:45 -0300

    Correção ex5

--- python/lista-3/exercicio5.py
+++ python/lista-3/exercicio5.py
@@ -1,12 +1,12 @@
-dias_uteis = 20
-salario = int(input('Salário: ')
-horas_trabalhadas = int(input('Horas trabalhadas: '))
-salario_por_hora = int(input('Salário por hora: '))
+dias_uteis = 22
+salario = float(input('Salário: '))
+horas_trabalhadas = float(input('Horas extras trabalhadas: ')) # horas extras trabalhadas
+salario_por_hora = float(input('Salário por hora: '))
 hora_extra = (salario * 1.5) * horas_trabalhadas
 
 if (horas_trabalhadas > 0):
-    salario_final = salario + hora_extra
-    print('O salário final é igual a R$ ' + salario_final)
- else:
-    salario_final = salario
-    print('O salário final é igual a R$ ' + salario_final)
+   salario_final = salario + hora_extra
+   print('O salário final é igual a R$ ' + str(salario_final))
+else:
+   salario_final = salario
+   print('O salário final é igual a R$ ' + str(salario_final))
