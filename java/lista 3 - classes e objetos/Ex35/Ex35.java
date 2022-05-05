f7c0aa5c7337e3d44a6110a0dd098ab5c0fa2831
Gabriela Costa <gabrielasilvaalvescosta@gmail.com>
2022-05-05 00:57:04 -0300

    Exercicio 35

--- java/lista 3 - classes e objetos/Ex35/Ex35.java
+++ java/lista 3 - classes e objetos/Ex35/Ex35.java
@@ -28,9 +28,10 @@ public class Ex35 {
 		String agencia;
 		String numero;
 		double saldo;
+
 		
 		for (int i = 0; i <= 4; i++) {
-			
+		
 			Cliente cliente = new Cliente();
 			
 			System.out.printf("Digite o id: ");
@@ -45,27 +46,52 @@ public class Ex35 {
 			idade = ler.nextInt();
 			cliente.setIdade(idade);
 			
-			System.out.printf("Digite o email");
+			System.out.printf("Digite o email: ");
 			email = ler.next();
 			cliente.setEmail(email);
 			
-			System.out.printf("Tem conta bancária S/N");
+			System.out.printf("Tem conta bancária? S/N");
 			resposta = ler.next().charAt(0);
 			
-			if (resposta == 'S') {
+			if (resposta == 'S' || resposta == 's') {
 				
 				Conta conta = new Conta();
 				
 				System.out.printf("Digite a agencia: ");
 				agencia = ler.next();
-				conta.setAgencia();
+				conta.setAgencia(agencia);
+				
+				System.out.printf("Digite o numero: ");
+				numero = ler.next();
+				conta.setNumero(numero);
+				
+				System.out.printf("Digite o saldo: ");
+				saldo = ler.nextDouble();
+				conta.setSaldo(saldo);
+				
+				cliente = new Cliente(id, nome, idade, email, conta);
+				
+			} else {
+				
+				cliente = new Cliente(id, nome, idade, email, null);
 				
 			}
 			
+			listaCliente[i] = cliente;
+			
+		}
+		
+		// exibir clientes
+		
+		for (int i = 0; i <= 4; i++) {
+			
+			
+			System.out.printf("\n Dados do cliente %d \n Nome: %s \n Idade: %d \n Saldo: %s", listaCliente[i].getId(), listaCliente[i].getNome(), listaCliente[i].getIdade(), listaCliente[i].getConta().getSaldo());
 			
 		}
 		
 		
+
 		ler.close();
 
 	}
